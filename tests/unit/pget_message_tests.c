282b41bca06d134e8f96147d230f5b8a37d856b5
Ben Johnson <benbjohnson@yahoo.com>
2012-11-10 21:05:05 -0700

    Convert 'get_property' to use message handler.

--- tests/unit/pget_message_tests.c
+++ tests/unit/pget_message_tests.c
@@ -48,20 +48,21 @@ int test_sky_get_property_message_unpack() {
 
 int test_sky_get_property_message_process() {
     loadtmp("tests/fixtures/get_property_message/1/table");
+    sky_server *server = sky_server_create(NULL);
+    sky_message_header *header = sky_message_header_create();
     sky_table *table = sky_table_create();
     table->path = bfromcstr("tmp");
     sky_table_open(table);
     
-    sky_get_property_message *message = sky_get_property_message_create();
-    message->property_id = 1;
-
+    FILE *input = fopen("tests/fixtures/get_property_message/1/input", "r");
     FILE *output = fopen("tmp/output", "w");
-    mu_assert(sky_get_property_message_process(message, table, output) == 0, "");
-    fclose(output);
+    int rc = sky_get_property_message_process(server, header, table, input, output);
+    mu_assert_int_equals(rc, 0);
     mu_assert_file("tmp/output", "tests/fixtures/get_property_message/1/output");
 
-    sky_get_property_message_free(message);
     sky_table_free(table);
+    sky_message_header_free(header);
+    sky_server_free(server);
     return 0;
 }
 
