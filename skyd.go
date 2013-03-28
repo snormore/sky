eeeb397348bff5e9abc4d96ab7e468cb07bfc782
Ben Johnson <benbjohnson@yahoo.com>
2013-03-27 22:18:01 -0600

    Add pid file and signal handler.

--- skyd.go
+++ skyd.go
@@ -4,7 +4,10 @@ import (
 	"./skyd"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"runtime"
+	"os/signal"
 )
 
 //------------------------------------------------------------------------------
@@ -23,6 +26,10 @@ const (
 	dataDirUsage = "the data directory"
 )
 
+const (
+	pidPath = "/var/run/skyd.pid"
+)
+
 //------------------------------------------------------------------------------
 //
 // Variables
@@ -38,12 +45,20 @@ var dataDir string
 //
 //------------------------------------------------------------------------------
 
+//--------------------------------------
+// Initialization
+//--------------------------------------
+
 func init() {
 	flag.UintVar(&port, "port", defaultPort, portUsage)
 	flag.UintVar(&port, "p", defaultPort, portUsage+"(shorthand)")
 	flag.StringVar(&dataDir, "data-dir", defaultDataDir, dataDirUsage)
 	flag.StringVar(&dataDir, "d", defaultDataDir, dataDirUsage+"(shorthand)")
 }
+
+//--------------------------------------
+// Main
+//--------------------------------------
 
 func main() {
 	// Parse the command line arguments.
@@ -52,13 +67,58 @@ func main() {
 	// Hardcore parallelism right here.
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	
+	// Initialize
+	server := skyd.NewServer(port, dataDir)
+	writePidFile()
+	setupSignalHandlers(server)
+	
 	// Start the server up!
 	c := make(chan bool)
-	server := skyd.NewServer(port, dataDir)
 	err := server.ListenAndServe(c)
 	if err != nil {
 		fmt.Printf("%v\n", err)
 		return
 	}
 	<- c
+
+	// Remove PID file.
+	deletePidFile()
+}
+
+//--------------------------------------
+// Signals
+//--------------------------------------
+
+// Handles signals received from the OS.
+func setupSignalHandlers(server *skyd.Server) {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func(){
+	    for _ = range c {
+			fmt.Fprintln(os.Stderr, "Shutting down...")
+			server.Shutdown()
+			deletePidFile();
+	    }
+	}()
+}
+
+//--------------------------------------
+// PID
+//--------------------------------------
+
+// Writes a file to /var/run that contains the current process id.
+func writePidFile() {
+	pid := fmt.Sprintf("%d", os.Getpid())
+	if err := ioutil.WriteFile(pidPath, []byte(pid), 0644); err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to write pid file: %v\n", err)
+	}
+}
+
+// Deletes the pid file.
+func deletePidFile() {
+	if _, err := os.Stat(pidPath); !os.IsNotExist(err) {
+		if err = os.Remove(pidPath); err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to remove pid file: %v\n", err)
+		}
+	}
 }
