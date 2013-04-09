63c6c0d4dc0125c3de1617ab35c7d64fc09c277e
Ben Johnson <benbjohnson@yahoo.com>
2013-04-08 22:50:23 -0600

    Add config file support.

--- skyd.go
+++ skyd.go
@@ -12,32 +12,12 @@ import (
 
 //------------------------------------------------------------------------------
 //
-// Constants
-//
-//------------------------------------------------------------------------------
-
-const (
-	defaultPort    = 8585
-	defaultDataDir = "/var/lib/sky"
-)
-
-const (
-	portUsage    = "the port to listen on"
-	dataDirUsage = "the data directory"
-)
-
-const (
-	pidPath = "/var/run/skyd.pid"
-)
-
-//------------------------------------------------------------------------------
-//
 // Variables
 //
 //------------------------------------------------------------------------------
 
-var port uint
-var dataDir string
+var config *skyd.Config
+var configPath string
 
 //------------------------------------------------------------------------------
 //
@@ -50,10 +30,12 @@ var dataDir string
 //--------------------------------------
 
 func init() {
-	flag.UintVar(&port, "port", defaultPort, portUsage)
-	flag.UintVar(&port, "p", defaultPort, portUsage+"(shorthand)")
-	flag.StringVar(&dataDir, "data-dir", defaultDataDir, dataDirUsage)
-	flag.StringVar(&dataDir, "d", defaultDataDir, dataDirUsage+"(shorthand)")
+ 	config = skyd.NewConfig()
+	flag.UintVar(&config.Port, "port", config.Port, "the port to listen on")
+	flag.UintVar(&config.Port, "p", config.Port, "the port to listen on")
+	flag.StringVar(&config.DataPath, "data-path", config.DataPath, "the data directory")
+	flag.StringVar(&config.PidPath, "pid-path", config.PidPath, "the path to the pid file")
+	flag.StringVar(&configPath, "config", "", "the path to the config file")
 }
 
 //--------------------------------------
@@ -61,16 +43,28 @@ func init() {
 //--------------------------------------
 
 func main() {
-	// Parse the command line arguments.
+	// Parse the command line arguments and load the config file (if specified).
 	flag.Parse()
+	if configPath != "" {
+		file, err := os.Open(configPath)
+		if err != nil {
+			fmt.Printf("Unable to open config: %v\n", err)
+			return
+		}
+		defer file.Close()
+		if err = config.Decode(file); err != nil {
+			fmt.Printf("Unable to parse config: %v\n", err)
+			os.Exit(1)
+		}
+	}
 
 	// Hardcore parallelism right here.
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
 	// Initialize
-	server := skyd.NewServer(port, dataDir)
+	server := skyd.NewServer(config.Port, config.DataPath)
 	writePidFile()
-	//setupSignalHandlers(server)
+	setupSignalHandlers(server)
 
 	// Start the server up!
 	c := make(chan bool)
@@ -117,15 +111,15 @@ func cleanup(server *skyd.Server) {
 // Writes a file to /var/run that contains the current process id.
 func writePidFile() {
 	pid := fmt.Sprintf("%d", os.Getpid())
-	if err := ioutil.WriteFile(pidPath, []byte(pid), 0644); err != nil {
+	if err := ioutil.WriteFile(config.PidPath, []byte(pid), 0644); err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to write pid file: %v\n", err)
 	}
 }
 
 // Deletes the pid file.
 func deletePidFile() {
-	if _, err := os.Stat(pidPath); !os.IsNotExist(err) {
-		if err = os.Remove(pidPath); err != nil {
+	if _, err := os.Stat(config.PidPath); !os.IsNotExist(err) {
+		if err = os.Remove(config.PidPath); err != nil {
 			fmt.Fprintf(os.Stderr, "Unable to remove pid file: %v\n", err)
 		}
 	}
