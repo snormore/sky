30a586e034d1b18527bb900b331666943a0b1fbf
Ben Johnson <benbjohnson@yahoo.com>
2013-11-26 15:31:09 +0000

    Codegen

---
+++ query/codegen/codegen.go
@@ -0,0 +1,10 @@
+package codegen
+
+import (
+	// TODO: Import gollvm.
+)
+
+// Codegen generates a runnable LLVM execution engine for a query.
+func Codegen() {
+	// TODO: Create a visitor to iterate over the entire AST.
+}
